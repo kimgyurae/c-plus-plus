e9592f93a319d6bdf9f9060893d232a0d03a7d76
Gyurae Kim <gxk32@hitit.xyz>
2021-05-31 15:42:25 +0900

    added read me

---
+++ inputProgram.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string first_name;
+    string last_name;
+
+    cout<<"Enter your first name: ";
+    cin>>first_name;
+
+    cout<<"Enter your last name: ";
+    cin>>last_name;
+
+    cout<<"your full name is: " << first_name << " " << last_name << endl;
+    return 0;
+}
